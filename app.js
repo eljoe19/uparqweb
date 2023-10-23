2267399392b0f568e4c1c5e4734dc28daebdded1
eljoe19 <joe_tmar@hotmail.com>
2023-10-23 20:02:27 -0300

    final test

--- app.js
+++ app.js
@@ -8,23 +8,17 @@ const content = 'Contenido del archivo de text';
 app.get('/', (req, res) => {
    
       //el archivo persiste entre requests
-    fs.writeFile('/home/joe/test.txt', content, err => {
+    fs.writeFile('test.txt', content, err => {
       if (err) {
         res.send('ERROR AL ESCRIBIR EL ARCHIVO! '+ err);
-        //console.error(err);
       }
       // file written successfully
-      //res.send('Hola Mundo Success!');
       fs.readFile('test.txt', 'utf8', function(err, data){ 
-        // Display the file content 
-        res.send(data);
-    }); 
+          // Display the file content 
+         res.send(data);
+      }); 
 
     });
-
-
-
-   // res.send('Hola Mundo Final!');
     
   });
 
