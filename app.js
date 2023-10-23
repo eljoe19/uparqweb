01fd6f7cd4ea09db3cfa1e9382ab96cf940e5683
eljoe19 <joe_tmar@hotmail.com>
2023-10-23 20:38:35 -0300

    method

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
       // file written successfully
       fs.readFile('test.txt', 'utf8', function(err, data){ 
           // Display the file content 
-          res.status(201).send(data);
+          res.status(201).send(data + ' ' + req.method);
       }); 
 
     });
