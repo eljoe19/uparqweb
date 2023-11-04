34491f5a340fdb4e25612bd0e34b186fba225c98
eljoe19 <joe_tmar@hotmail.com>
2023-11-04 18:27:16 -0300

    filesync

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.post('/objetos',(req,res)=>{
   }else{
     //guardo nuevo objeto en txt
     var valorTest=guardarNuevoObjeto2();
-    var valorRes= await guardarNuevoObjeto(req.query.codigo,req.query.nombre,req.query.desc,req.query.categoria);
+    var valorRes=guardarNuevoObjeto(req.query.codigo,req.query.nombre,req.query.desc,req.query.categoria);
     if(valorRes){
       res.status(200);
       return res.send("OK - ValorRes: " + valorRes + ' ValorTest: ' + valorTest);
@@ -29,17 +29,9 @@ app.post('/objetos',(req,res)=>{
 function guardarNuevoObjeto2(){
     return true;
 };
-async function guardarNuevoObjeto(codigo,nombre,desc,categoria){
-  
+function guardarNuevoObjeto(codigo,nombre,desc,categoria){
   nuevoObj= codigo + ',' + nombre + ',' + desc + ',' + categoria;
-  await fs.writeFile(pathArchivoTXT, nuevoObj, err => {
-    if (err) {
-      //res.send('ERROR AL ESCRIBIR EL ARCHIVO! '+ err);
-      return false;
-    }
-    //file written successfully
-      return true; 
-  });
+  fs.writeFileSync(pathArchivoTXT, nuevoObj);
 };
 
 
