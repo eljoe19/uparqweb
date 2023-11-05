5bfb2ce98fd6e2d7f77db2c11d6af25fa985a045
eljoe19 <joe_tmar@hotmail.com>
2023-11-05 17:45:28 -0300

    deletetest

--- app.js
+++ app.js
@@ -106,9 +106,8 @@ app.delete('/objetos/:id',(req,res)=>{
     {
       //verifico si es el objeto del request para no incluirlo en la nueva DB
       let tmp=arrObjetos[i].split(',');
-      guardarNuevoObjeto(tmp[0],tmp[1],tmp[2],tmp[3]);
-      if(!tmp[0] === req.params.id){
-        //lo agrego a la DB
+      if(tmp[0] !== req.params.id){
+        //si no es el objeto del request, lo agrego a la DB
         guardarNuevoObjeto(tmp[0],tmp[1],tmp[2],tmp[3]);
       }
     }
